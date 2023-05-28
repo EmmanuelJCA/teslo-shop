0788555ea9ca6322b111929f0ed8c3da37cd711e
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-28 18:46:07 -0400

    files endpoint

---
+++ src/files/files.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+
+  constructor(
+    private readonly filesService: FilesService
+  ) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor)
+  uploadProductImage(
+   @UploadedFile() file: Express.Multer.File
+  ) {
+    return file
+  }
+
+}
