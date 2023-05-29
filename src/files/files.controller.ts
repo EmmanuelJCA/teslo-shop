ce4dc8f286c9a3b2b6d5631826b4635488e458c0
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-28 21:28:00 -0400

    file endpoint

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,20 +1,48 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Response } from 'express';
+import { Controller, Get, HttpStatus, Param, ParseFilePipeBuilder, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
 
   constructor(
-    private readonly filesService: FilesService
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
   ) {}
 
-  @Post('product')
-  @UseInterceptors(FileInterceptor)
-  uploadProductImage(
-   @UploadedFile() file: Express.Multer.File
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
   ) {
-    return file
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }))
+  uploadProductImage(
+   @UploadedFile(
+    new ParseFilePipeBuilder()
+      .addFileTypeValidator({
+        fileType: /(jpg|jpeg|png)$/,
+      })
+      .build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+      })
+   ) file: Express.Multer.File,
+  ) {
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+    return secureUrl;
   }
 
 }
